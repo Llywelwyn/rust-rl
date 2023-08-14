1319ed16e0775a5f5e6a98584a69d670271bf136
Llywelwyn <lewiiswynne@gmail.com>
2023-08-14 07:04:27 +0100

    capitalises unid scrolls

--- src/map/dungeon.rs
+++ src/map/dungeon.rs
@@ -66,37 +66,37 @@ fn make_scroll_name(rng: &mut RandomNumberGenerator) -> (String, String) {
     for i in 0..len {
         if i % 2 == 0 {
             let char = match rng.roll_dice(1, 5) {
-                1 => "a",
-                2 => "e",
-                3 => "i",
-                4 => "o",
-                _ => "u",
+                1 => "A",
+                2 => "E",
+                3 => "I",
+                4 => "O",
+                _ => "U",
             };
             singular += char;
             plural += char;
         } else {
             let char = match rng.roll_dice(1, 21) {
-                1 => "b",
-                2 => "c",
-                3 => "d",
-                4 => "f",
-                5 => "g",
-                6 => "h",
-                7 => "j",
-                8 => "k",
-                9 => "l",
-                10 => "m",
-                11 => "n",
-                12 => "p",
-                13 => "q",
-                14 => "r",
-                15 => "s",
-                16 => "t",
-                17 => "v",
-                18 => "w",
-                19 => "x",
-                20 => "y",
-                _ => "z",
+                1 => "B",
+                2 => "C",
+                3 => "D",
+                4 => "F",
+                5 => "G",
+                6 => "H",
+                7 => "J",
+                8 => "K",
+                9 => "L",
+                10 => "M",
+                11 => "N",
+                12 => "P",
+                13 => "Q",
+                14 => "R",
+                15 => "S",
+                16 => "T",
+                17 => "V",
+                18 => "W",
+                19 => "X",
+                20 => "Y",
+                _ => "Z",
             };
             singular += char;
             plural += char;
