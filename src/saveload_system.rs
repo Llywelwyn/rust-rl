9c8f3014911730cd402ae2842fbbb4b7a32cb724
Llywelwyn <lewiiswynne@gmail.com>
2023-10-24 23:32:42 +0100

    Infallible -> NoError

--- src/saveload_system.rs
+++ src/saveload_system.rs
@@ -8,15 +8,15 @@ use specs::saveload::{
     SimpleMarker,
     SimpleMarkerAllocator,
 };
+use specs::error::NoError;
 use std::fs;
 use std::fs::File;
 use std::path::Path;
-use std::convert::Infallible;
 
 macro_rules! serialize_individually {
     ($ecs:expr, $ser:expr, $data:expr, $($type:ty),*) => {
         $(
-        SerializeComponents::<Infallible, SimpleMarker<SerializeMe>>::serialize(
+        SerializeComponents::<NoError, SimpleMarker<SerializeMe>>::serialize(
             &( $ecs.read_storage::<$type>(), ),
             &$data.0,
             &$data.1,
@@ -156,7 +156,7 @@ pub fn does_save_exist() -> bool {
 macro_rules! deserialize_individually {
     ($ecs:expr, $de:expr, $data:expr, $($type:ty),*) => {
         $(
-        DeserializeComponents::<Infallible, _>::deserialize(
+        DeserializeComponents::<NoError, _>::deserialize(
             &mut ( &mut $ecs.write_storage::<$type>(), ),
             &$data.0, // entities
             &mut $data.1, // marker
